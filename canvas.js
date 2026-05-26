c77ac553e9e18794c67d91fc21d766d8805b3bee
lRethi <azraethly@gmail.com>
2026-05-26 10:53:56 -0300

    ronaldo

--- canvas.js
+++ canvas.js
@@ -16,18 +16,31 @@ var gravidadeValor = 0.5;
 var velGravidadeY = 0;
 var onGround = false;
 
-var colisoes = [
-    { x: 0, y: 600, width: 800, height: 1, cor: "#000000" },
-    { x: 0, y: 0, width: 800, height: 1, cor: "#000000" },
-    { x: 800, y: 0, width: 1, height: 600, cor: "#000000" },
-    { x: 0, y: 0, width: 1, height: 600, cor: "#000000" },
+var mouseX = 0;
+var mouseY = 0;
+var x = 250, y = 150;
 
-    { x: 100, y: 400, width: 200, height: 5, cor: "#005fb8" },
-    { x: 400, y: 300, width: 200, height: 5, cor: "#00B806" },
-    { x: 300, y: 200, width: 100, height: 5, cor: "#00B806" },
-    { x: 350, y: 100, width: 100, height: 5, cor: "#00B806" },
-    { x: 350, y: 350, width: 20, height: 200, cor: "#00B806" }
+var gameMap = [
+    { x: 0, y: 600, width: 800, height: 1, cor: "#000000", colPlayer: true, colMouse: true },
+    { x: 0, y: 0, width: 800, height: 1, cor: "#000000", colPlayer: true, colMouse: true },
+    { x: 800, y: 0, width: 1, height: 600, cor: "#000000", colPlayer: true, colMouse: true },
+    { x: 0, y: 0, width: 1, height: 600, cor: "#000000", colPlayer: true, colMouse: true },
+
+    { x: 100, y: 400, width: 200, height: 5, cor: "#00b80f", colPlayer: true, colMouse: true },
+    { x: 400, y: 300, width: 200, height: 5, cor: "#00b81f", colPlayer: true, colMouse: true },
+    { x: 300, y: 200, width: 100, height: 5, cor: "#00B806", colPlayer: true, colMouse: true },
+    { x: 350, y: 100, width: 100, height: 5, cor: "#b86800", colPlayer: true, colMouse: false },
+    { x: 350, y: 350, width: 20, height: 200, cor: "#b80099", colPlayer: false, colMouse: true },
+
+    { x: 350, y: 350, width: 20, height: 20, cor: "#000000", colPlayer: true, colMouse: true },
+    { x: 350, y: 530, width: 20, height: 20, cor: "#000000", colPlayer: true, colMouse: true }
 ];
+
+canvas.addEventListener("mousemove", function (event) {
+    const rect = canvas.getBoundingClientRect();
+    mouseX = event.clientX - rect.left;
+    mouseY = event.clientY - rect.top;
+});
 
 document.addEventListener("keydown", (event) => {
     if (event.key === "ArrowUp" && onGround) {
@@ -48,21 +61,71 @@ function logicaBase() {
     c.clearRect(0, 0, canvas.width, canvas.height);
     desenharMapa();
     movimentoBase();
+    mouseBolinha();
 }
 
 function desenharMapa() {
-    for (let bloco of colisoes) {
+    for (let bloco of gameMap) {
         c.fillStyle = bloco.cor;
         c.fillRect(bloco.x, bloco.y, bloco.width, bloco.height);
     }
 }
 
-function colisaoAABB(xQuadrado, yQuadrado, wQuadrado, hQuadrado, xCenario, yCenario, wCenario, hCenario) {
+function colisaoAABB(xQuadrado, yQuadrado, wQuadrado, hQuadrado, xCenario, yCenario, wCenario, hCenario, podeColidir) {
     return xQuadrado < xCenario + wCenario && // se a borda esquerda do player tá antes da borda direita do cenário
         xQuadrado + wQuadrado > xCenario && // se a borda direita do player passou da borda esquerda do cenário
         yQuadrado < yCenario + hCenario && // se o topo do player está acima da base do cenário
-        yQuadrado + hQuadrado > yCenario; // se a base do player está acima do topo do cenário
+        yQuadrado + hQuadrado > yCenario && // se a base do player está acima do topo do cenário
+        podeColidir == true;
     // se tudo for verdadeiro, ent significa que tá colidindo
+}
+
+function mouseBolinha() {
+    let raio = 10;
+
+    let nextX = x + (mouseX - x) * 0.1;
+    let nextY = y + (mouseY - y) * 0.1;
+
+    for (let bloco of gameMap) {
+        if (!bloco.colMouse) continue;
+
+        if (colisaoAABB(
+            nextX - raio, y - raio, raio * 2, raio * 2,
+            bloco.x, bloco.y, bloco.width, bloco.height,
+            true
+        )) {
+            if (nextX > x) {
+                nextX = bloco.x - raio - 0.001;
+            } else if (nextX < x) {
+                nextX = bloco.x + bloco.width + raio + 0.001;
+            }
+        }
+    }
+
+    x = nextX;
+
+    for (let bloco of gameMap) {
+        if (!bloco.colMouse) continue;
+
+        if (colisaoAABB(
+            x - raio, nextY - raio, raio * 2, raio * 2,
+            bloco.x, bloco.y, bloco.width, bloco.height,
+            true
+        )) {
+            if (nextY > y) {
+                nextY = bloco.y - raio - 0.001;
+            } else if (nextY < y) {
+                nextY = bloco.y + bloco.height + raio + 0.001;
+            }
+        }
+    }
+
+    y = nextY;
+
+    c.beginPath();
+    c.arc(x, y, raio, 0, Math.PI * 2);
+    c.fillStyle = "#00c3ff";
+    c.fill();
 }
 
 function movimentoBase() {
@@ -75,8 +138,8 @@ function movimentoBase() {
         nextX += velocidadeX;
     }
 
-    for (let bloco of colisoes) {
-        if (colisaoAABB(nextX, posYQuadrado1, tam, tam, bloco.x, bloco.y, bloco.width, bloco.height)) {
+    for (let bloco of gameMap) {
+        if (colisaoAABB(nextX, posYQuadrado1, tam, tam, bloco.x, bloco.y, bloco.width, bloco.height, bloco.colPlayer)) {
             if (nextX > posXQuadrado1) {
                 nextX = bloco.x - tam;
             } else if (nextX < posXQuadrado1) {
@@ -92,8 +155,8 @@ function movimentoBase() {
 
     onGround = false;
 
-    for (let bloco of colisoes) {
-        if (colisaoAABB(posXQuadrado1, nextY, tam, tam, bloco.x, bloco.y, bloco.width, bloco.height)) {
+    for (let bloco of gameMap) {
+        if (colisaoAABB(posXQuadrado1, nextY, tam, tam, bloco.x, bloco.y, bloco.width, bloco.height, bloco.colPlayer)) {
             if (velGravidadeY > 0) {
                 nextY = bloco.y - tam;
                 velGravidadeY = 0;
@@ -107,8 +170,25 @@ function movimentoBase() {
 
     posYQuadrado1 = nextY;
 
-    c.fillStyle = "#f4f800c4";
+    desenharPlayer();
+}
+function desenharPlayer() {
+    c.fillStyle = "#f4f800";
     c.fillRect(posXQuadrado1, posYQuadrado1, tam, tam);
+    for (var i = 1; i <= 2; i++) {
+        c.fillStyle = "#000000";
+        switch (i) {
+            case 1:
+                c.fillRect(posXQuadrado1 + 10, posYQuadrado1 + 15, 5, 5);
+                break;
+            case 2:
+                c.fillRect(posXQuadrado1 + 35, posYQuadrado1 + 15, 5, 5);
+                break;
+        }
+    }
+    c.fillRect(posXQuadrado1 + 10, posYQuadrado1 + 35, 30, 5);
+    c.fillRect(posXQuadrado1 + 5, posYQuadrado1 + 30, 5, 5);
+    c.fillRect(posXQuadrado1 + 40, posYQuadrado1 + 30, 5, 5);
 }
 
 function mouseBotao() {
