81894d42c438f2244333f1860113274811dcd47f
lRethi <azraethly@gmail.com>
2026-05-28 08:50:13 -0300

    aaaa

--- canvas.js
+++ canvas.js
@@ -18,23 +18,27 @@ var onGround = false;
 
 var mouseX = 0;
 var mouseY = 0;
+var mouseClick = false;
 var x = 250, y = 150;
 
 var gameMap = [
-    { id: "bordaDireita", x: 0, y: 600, width: 800, height: 1, cor: "#000000", colPlayer: true, colMouse: true, apertavel: false, targetId: null },
-    { id: "bordaEsquerda", x: 0, y: 0, width: 800, height: 1, cor: "#000000", colPlayer: true, colMouse: true, apertavel: false, targetId: null },
-    { id: "bordaInferior", x: 800, y: 0, width: 1, height: 600, cor: "#000000", colPlayer: true, colMouse: true, apertavel: false, targetId: null },
-    { id: "bordaSuperior", x: 0, y: 0, width: 1, height: 600, cor: "#000000", colPlayer: true, colMouse: true, apertavel: false, targetId: null },
+    { id: "bordaDireita", x: 0, y: 600, width: 800, height: 1, cor: "#000000", colPlayer: true, colMouse: true, clickableMouse: false, targetId: null, botApertado: null, objAtivo: true, playerClickable: false },
+    { id: "bordaEsquerda", x: 0, y: 0, width: 800, height: 1, cor: "#000000", colPlayer: true, colMouse: true, clickableMouse: false, targetId: null, botApertado: null, objAtivo: true, playerClickable: false },
+    { id: "bordaInferior", x: 800, y: 0, width: 1, height: 600, cor: "#000000", colPlayer: true, colMouse: true, clickableMouse: false, targetId: null, botApertado: null, objAtivo: true, playerClickable: false },
+    { id: "bordaSuperior", x: 0, y: 0, width: 1, height: 600, cor: "#000000", colPlayer: true, colMouse: true, clickableMouse: false, targetId: null, botApertado: null, objAtivo: true, playerClickable: false },
 
-    { id: "caminho1", x: 100, y: 400, width: 200, height: 5, cor: "#00b80f", colPlayer: true, colMouse: true, apertavel: false, targetId: null },
-    { id: "caminho2", x: 400, y: 300, width: 200, height: 5, cor: "#00b81f", colPlayer: true, colMouse: true, apertavel: false, targetId: null },
-    { id: "caminho3", x: 300, y: 200, width: 100, height: 5, cor: "#00B806", colPlayer: true, colMouse: true, apertavel: false, targetId: null },
-    
-    { id: "mouseTravessa1", x: 350, y: 100, width: 100, height: 5, cor: "#b86800", colPlayer: true, colMouse: false, apertavel: false, targetId: null },
-    { id: "cuboTravessa1", x: 350, y: 350, width: 20, height: 200, cor: "#b80099", colPlayer: false, colMouse: true, apertavel: false, targetId: null },
+    { id: "caminho1", x: 100, y: 400, width: 200, height: 5, cor: "#00b80f", colPlayer: true, colMouse: true, clickableMouse: false, targetId: null, botApertado: null, objAtivo: true, playerClickable: false },
+    { id: "caminho2", x: 400, y: 300, width: 200, height: 5, cor: "#00b81f", colPlayer: true, colMouse: true, clickableMouse: false, targetId: null, botApertado: null, objAtivo: true, playerClickable: false },
+    { id: "caminho3", x: 300, y: 200, width: 100, height: 5, cor: "#00B806", colPlayer: true, colMouse: true, clickableMouse: false, targetId: null, botApertado: null, objAtivo: true, playerClickable: false },
 
-    { id: "bordaTravessa1", x: 350, y: 350, width: 20, height: 20, cor: "#000000", colPlayer: true, colMouse: true, apertavel: false, targetId: null },
-    { id: "bordaTravessa2", x: 350, y: 530, width: 20, height: 20, cor: "#000000", colPlayer: true, colMouse: true, apertavel: false, targetId: null }
+    { id: "mouseTravessa1", x: 350, y: 100, width: 100, height: 5, cor: "#b86800", colPlayer: true, colMouse: false, clickableMouse: false, targetId: null, botApertado: null, objAtivo: true, playerClickable: false },
+    { id: "cuboTravessa1", x: 350, y: 350, width: 20, height: 200, cor: "#b80099", colPlayer: false, colMouse: true, clickableMouse: false, targetId: null, botApertado: null, objAtivo: true, playerClickable: false },
+
+    { id: "bordaTravessa1", x: 350, y: 350, width: 20, height: 20, cor: "#000000", colPlayer: true, colMouse: true, clickableMouse: false, targetId: null, botApertado: null, objAtivo: true, playerClickable: false },
+    { id: "bordaTravessa2", x: 350, y: 530, width: 20, height: 20, cor: "#000000", colPlayer: true, colMouse: true, clickableMouse: false, targetId: null, botApertado: null, objAtivo: true, playerClickable: false },
+
+    { id: "botaoIrado", x: 150, y: 200, width: 40, height: 40, cor: "#ff0000", colPlayer: false, colMouse: false, clickableMouse: true, targetId: "cuboTravessa1", botApertado: false, objAtivo: true, playerClickable: false },
+    { id: "botaoIradoPlayer", x: 300, y: 200, width: 40, height: 40, cor: "#ff0000", colPlayer: false, colMouse: false, clickableMouse: false, targetId: "cuboTravessa1", botApertado: false, objAtivo: true, playerClickable: true },
 ];
 
 canvas.addEventListener("mousemove", function (event) {
@@ -44,7 +48,7 @@ canvas.addEventListener("mousemove", function (event) {
 });
 
 document.addEventListener("keydown", (event) => {
-    if (event.key === "ArrowUp" && onGround) {
+    if ((event.key === "ArrowUp" && onGround) || (event.key === "w" && onGround) || (event.key === " " && onGround)) {
         velGravidadeY = -velocidadeY;
         onGround = false;
     }
@@ -54,6 +58,14 @@ document.addEventListener("keydown", (event) => {
 document.addEventListener("keyup", (event) => {
     teclas[event.key] = false;
 });
+
+document.addEventListener("mousedown", (event) => {
+    mouseClick = true;
+})
+
+document.addEventListener("mouseup", (event) => {
+    mouseClick = false;
+})
 
 logicaBase();
 
@@ -67,6 +79,8 @@ function logicaBase() {
 
 function desenharMapa() {
     for (let bloco of gameMap) {
+        if (!bloco.objAtivo) continue;
+
         c.fillStyle = bloco.cor;
         c.fillRect(bloco.x, bloco.y, bloco.width, bloco.height);
     }
@@ -81,6 +95,18 @@ function colisaoAABB(xQuadrado, yQuadrado, wQuadrado, hQuadrado, xCenario, yCena
     // se tudo for verdadeiro, ent significa que tá colidindo
 }
 
+function ativarBotao(botao) {
+    if (botao.botApertado) return;
+    botao.botApertado = true;
+    botao.cor = "#5f5f5f"
+
+    for (let bloco of gameMap) {
+        if (bloco.id === botao.targetId) {
+            bloco.objAtivo = false;
+        }
+    }
+}
+
 function mouseBolinha() {
     let raio = 10;
 
@@ -88,11 +114,7 @@ function mouseBolinha() {
     let nextY = y + (mouseY - y) * 0.1;
 
     for (let bloco of gameMap) {
-        if (!bloco.colMouse && !bloco.apertavel) continue;
-
-        if(bloco.apertavel){
-            gameMap = gameMap.filter(b => b.id !== bloco.targetId);
-        }
+        if (!bloco.colMouse || !bloco.objAtivo) continue;
 
         if (colisaoAABB(
             nextX - raio, y - raio, raio * 2, raio * 2,
@@ -110,12 +132,7 @@ function mouseBolinha() {
     x = nextX;
 
     for (let bloco of gameMap) {
-        if (!bloco.colMouse && !bloco.apertavel) continue;
-
-        if(bloco.apertavel){
-            gameMap = gameMap.filter(b => b.id !== bloco.targetId);
-        }
-    
+        if (!bloco.colMouse || !bloco.objAtivo) continue;
 
         if (colisaoAABB(
             x - raio, nextY - raio, raio * 2, raio * 2,
@@ -132,6 +149,21 @@ function mouseBolinha() {
 
     y = nextY;
 
+    for (let bloco of gameMap) {
+        if (!bloco.clickableMouse || !bloco.objAtivo) continue;
+
+        if (
+            mouseClick &&
+            colisaoAABB(
+                x - raio, y - raio, raio * 2, raio * 2,
+                bloco.x, bloco.y, bloco.width, bloco.height,
+                true
+            )
+        ) {
+            ativarBotao(bloco);
+        }
+    }
+
     c.beginPath();
     c.arc(x, y, raio, 0, Math.PI * 2);
     c.fillStyle = "#00c3ff";
@@ -141,14 +173,16 @@ function mouseBolinha() {
 function movimentoBase() {
     let nextX = posXQuadrado1;
 
-    if (teclas["ArrowLeft"]) {
+    if (teclas["ArrowLeft"] || teclas["a"]) {
         nextX -= velocidadeX;
     }
-    if (teclas["ArrowRight"]) {
+    if (teclas["ArrowRight"] || teclas["d"]) {
         nextX += velocidadeX;
     }
 
     for (let bloco of gameMap) {
+        if (!bloco.objAtivo) continue;
+
         if (colisaoAABB(nextX, posYQuadrado1, tam, tam, bloco.x, bloco.y, bloco.width, bloco.height, bloco.colPlayer)) {
             if (nextX > posXQuadrado1) {
                 nextX = bloco.x - tam;
@@ -166,6 +200,8 @@ function movimentoBase() {
     onGround = false;
 
     for (let bloco of gameMap) {
+        if (!bloco.objAtivo) continue;
+
         if (colisaoAABB(posXQuadrado1, nextY, tam, tam, bloco.x, bloco.y, bloco.width, bloco.height, bloco.colPlayer)) {
             if (velGravidadeY > 0) {
                 nextY = bloco.y - tam;
@@ -179,6 +215,20 @@ function movimentoBase() {
     }
 
     posYQuadrado1 = nextY;
+
+    for (let bloco of gameMap) {
+        if (!bloco.playerClickable || !bloco.objAtivo) continue;
+
+        if (
+            colisaoAABB(
+                posXQuadrado1, posYQuadrado1, tam, tam,
+                bloco.x, bloco.y, bloco.width, bloco.height,
+                true
+            )
+        ) {
+            ativarBotao(bloco);
+        }
+    }
 
     desenharPlayer();
 }
@@ -199,8 +249,4 @@ function desenharPlayer() {
     c.fillRect(posXQuadrado1 + 10, posYQuadrado1 + 35, 30, 5);
     c.fillRect(posXQuadrado1 + 5, posYQuadrado1 + 30, 5, 5);
     c.fillRect(posXQuadrado1 + 40, posYQuadrado1 + 30, 5, 5);
-}
-
-function mouseBotao() {
-
 }
